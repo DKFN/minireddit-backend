fac50957c38b5777f87ff418a35bbb029aee84d0
DKFN <valere.tetelin@gmail.com>
2019-03-17 23:36:26 +0100

    Version in app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send("Nothing here");
+  res.send("MiniReddit v 0.1");
 });
 
 app.use(function(err, req, res, next) {
